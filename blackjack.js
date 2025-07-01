7072e6d7d62664d7f5536e1be2d90bd6b584f36d
Vedansh <vvedansh7@gmail.com>
2025-07-01 21:41:55 +0530

    Add bet input and restart functionality

--- blackjack.js
+++ blackjack.js
@@ -13,6 +13,11 @@ let betamount = document.getElementById("bet-input")
 
 function startGame(){
     let enteredBet = parseInt(betamount.value)
+    if(isNaN(enteredBet) || enteredBet <= 0){
+        message = "Please enter a valid bet"
+        messageEl.textContent = message
+        return
+    }
     if(money >= enteredBet){
     currentBet = enteredBet
     money -= currentBet
@@ -21,16 +26,29 @@ function startGame(){
     let first = getRandomNum()
     let second = getRandomNum()
     cards = [first, second]
-    sum = first+second
+    sum = sum = first.value + second.value
+    checkAce()
     renderGame()
+    }
+    else{
+        messageEl.textContent = "Not enought amount in you wallet."
+        return
     }
 }
 
 function renderGame(){
-    cardsEl.textContent = "Cards: "
+    cardsEl.textContent = ""
     for(let i = 0; i < cards.length; i++){
-        cardsEl.textContent += cards[i] + " "
+        let img = document.createElement("img")
+        img.src = getCardImage(cards[i])
+        img.classList.add("card-img")
+        cardsEl.appendChild(img)
     }
+
+    // cardsEl.textContent = "Cards: "
+    // for(let i = 0; i < cards.length; i++){
+    //     cardsEl.textContent += cards[i] + " "
+    // }
     sumEl.textContent = "Sum: " + sum
 
     if(sum < 21){
@@ -39,7 +57,7 @@ function renderGame(){
     else if(sum === 21){
         message = "Congrats, its a blackjack!"
         alive = false
-        money += currentBet*2
+        money += currentBet*5
         purseEl.textContent = "Purse: " + money
     }
     else{
@@ -54,22 +72,35 @@ function newCard(){
     if (alive && sum < 21) {
         let newcard = getRandomNum()
         cards.push(newcard)
-        sum += newcard
+        sum += newcard.value
+        checkAce()
         renderGame()
     }
 }
 
 function getRandomNum(){
-    let randomNumber = Math.floor(Math.random()*13)+1;
-    if(randomNumber > 10){
-        return 10
-    }
-    else if(randomNumber === 1){
-        return 11
-    }
-    else{
-        return randomNumber
-    }
+    let ranks = [2,3,4,5,6,7,8,9,"J","Q","K","A"]
+    let suits = ["C", "D", "H", "S"]
+    let rank = ranks[Math.floor(Math.random()*ranks.length)]
+    let suit = suits[Math.floor(Math.random()*suits.length)]
+
+    let value
+    if(rank === "A") value = 11
+    else if(["J", "Q", "K"].includes(rank)) value = 10
+    else value = rank
+
+    return {rank: rank.toString(), suit, value}
+
+    // let randomNumber = Math.floor(Math.random()*13)+1;
+    // if(randomNumber > 10){
+    //     return 10
+    // }
+    // else if(randomNumber === 1){
+    //     return 11
+    // }
+    // else{
+    //     return randomNumber
+    // }
 }
 function addone(){
     money += 100
@@ -86,15 +117,27 @@ function addfive(){
 }
 
 function restartGame(){
-    money = 500
+    // money = 500
     cards = []
     sum = 0
     alive = false
     currentBet = 0
 
-    purseEl.textContent = "Purse: " + money
+    // purseEl.textContent = "Purse: " + money
     cardsEl.textContent = "Cards: "
     sumEl.textContent = "Sum: "
     messageEl.textContent = "Game reset. Place your bet to start"
     betamount.value = "";
+}
+
+function getCardImage(card) {
+    return `https://deckofcardsapi.com/static/img/${card.rank}${card.suit}.png`
+}
+function checkAce() {
+    for (let i = 0; i < cards.length; i++) {
+        if (sum > 21 && cards[i].rank === "A" && cards[i].value === 11) {
+            cards[i].value = 1
+            sum -= 10
+        }
+    }
 }
